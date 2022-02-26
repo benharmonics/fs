2d2d140d1cbb3b4b2223463f6e1ae7d786d4cead
benharmonics <benjamin.jiron+git@gmail.com>
2022-02-25 17:13:09 -0700

    Added filesize flag, human-readable filesizes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use clap::{arg, Command, ArgMatches};
+use clap::{arg, Arg, Command, ArgMatches};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
-use std::{fs, io, error};
+use std::{fs, io, cmp, error};
 use std::io::Write;
 use std::path::PathBuf;
 use std::collections::HashMap;
@@ -13,26 +13,38 @@ pub fn args() -> ArgMatches {
         .about("Reads items in a given directory")
         .arg(arg!(-a --all "Show hidden files"))
         .arg(arg!(-r --reverse "Reverse output order"))
+        .arg(arg!(-s --size "Get file size (in bytes)"))
+        .arg(Arg::new("human-readable")
+            .short('h')
+            .long("human-readable")
+            .help("With '-s', gives size in a human-readable format (kB, GB, etc)"))
         .arg(arg!([DIRECTORY] ... "One or more directories to read"))
         .get_matches()
+}
+
+/* Takes a file size in bytes and formats a string using the most 'readable' unit */
+fn pretty_filesize(num: u64) -> String {
+    let units = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    let delimiter = 1000_f64;
+    let exponent = cmp::min(((num as f64).ln() / delimiter.ln()).floor() as i32, (units.len() - 1) as i32);
+    let pretty_bytes = format!("{:.2}", (num as f64) / delimiter.powi(exponent)).parse::<f64>().unwrap() * 1_f64;
+    let unit = units[exponent as usize];
+    format!("{} {}", pretty_bytes, unit)
 }
 
 /* Reads the directory contents and prints them to stdout */
 fn write_to_stdout(stdout: &mut StandardStream, buf: PathBuf, flags: &HashMap<char, bool>)
                    -> Result<(), Box<dyn error::Error>> {
-    let mut all_entries: Vec<PathBuf> = fs::read_dir(buf.as_path())
+    let all_entries: Vec<PathBuf> = fs::read_dir(buf.as_path())
         .unwrap()
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<PathBuf>, io::Error>>()
         .unwrap_or(vec![]);
-    all_entries.sort();
-
-    // Reverse
-    if flags[&'r'] { all_entries.reverse(); }
 
     let mut dirs = Vec::new();
     let mut files = Vec::new();
 
+    // Separate all_entries into directories and files, to be printed separately
     for entry in all_entries {
         // Ignore hidden files
         if !flags[&'a'] && entry.file_name().unwrap().to_str().unwrap().starts_with('.') { continue; }
@@ -42,6 +54,9 @@ fn write_to_stdout(stdout: &mut StandardStream, buf: PathBuf, flags: &HashMap<ch
             files.push(entry);
         }
     }
+
+    // Reverse, if reverse flag is set
+    if flags[&'r'] { dirs.reverse(); files.reverse(); }
 
     // Get just the filename/dirname from each PathBuf and collect them into vectors
     let filenames: Vec<&str> = files.iter()
@@ -56,11 +71,23 @@ fn write_to_stdout(stdout: &mut StandardStream, buf: PathBuf, flags: &HashMap<ch
     for i in 0..dirs.len() {
         // Setting the correct color
         stdout.set_color(ColorSpec::new().set_fg(Some(Color::Blue)).set_bold(true))?;
+
+        // Getting file metadata
+        let attrs = dirs[i].metadata()?;
+        // Write file size to console if 's' flag used
+        if flags[&'s'] {
+            let size = if !flags[&'h'] { format!("{} B", attrs.len()) } else { pretty_filesize(attrs.len()) };
+            write!(&mut *stdout, "({}) ", size)?;
+        }
         writeln!(&mut *stdout, "{}", dirnames[i])?;
     }
     for i in 0..files.len() {
-        // Setting the correct color
         stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)))?;
+        let attrs = files[i].metadata()?;
+        if flags[&'s'] {
+            let size = if !flags[&'h'] { format!("{} B", attrs.len()) } else { pretty_filesize(attrs.len()) };
+            write!(&mut *stdout, "({}) ", size)?;
+        }
         writeln!(&mut *stdout, "{}", filenames[i])?;
     }
 
@@ -73,6 +100,8 @@ pub fn run(args: clap::ArgMatches) -> Result<(), Box<dyn error::Error>> {
     let flags = HashMap::from([
         ('a', args.is_present("all")),
         ('r', args.is_present("reverse")),
+        ('s', args.is_present("size")),
+        ('h', args.is_present("human-readable")),
     ]);
 
     // Set up stdout stream (as opposed to a buffer)
@@ -86,7 +115,7 @@ pub fn run(args: clap::ArgMatches) -> Result<(), Box<dyn error::Error>> {
     } else {
         for dir in dirs.unwrap().collect::<Vec<_>>() {
             let dir_path = fs::canonicalize(dir)?;
-            stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)))?;     // It can change
+            stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)))?;     // It can change
             writeln!(&mut stdout, " ==> {} <== ", dir_path.as_os_str().to_str().unwrap())?;
             write_to_stdout(&mut stdout, dir_path, &flags)?;
         }
